f6ae1d568b1a3630055f886b5a2155c527028c68
Nilay Arangil <50153327+nilayarangil@users.noreply.github.com>
2019-06-15 14:14:16 -0500

    initial commit
    raspberry based python program to turn fish lights on and off during specified timeframe

---
+++ fishtanklight.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO #Use GPIO library
+import datetime #Use datetime library to get current time
+
+GPIO.setwarnings(False)
+# Set time when to switch the light on
+timetoswitchon = datetime.datetime.now().replace(hour=12,minute=23,second=0,microsecond=0)
+# Set time when to switch the light off
+timetoswitchoff = datetime.datetime.now().replace(hour=12,minute=25,second=0,microsecond=0) 
+
+ledPin = 11    # pin11 is connected to the led anode (+ve pin)
+GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+GPIO.setup(ledPin, GPIO.OUT)   # Set ledPin's mode as output
+GPIO.output(ledPin, GPIO.HIGH) # Set ledPin high(+3.3V) to turn on led
+
+while True: # Continue looping indefinitely
+  now = datetime.datetime.now()    # Get the current time
+  GPIO.output(ledPin, GPIO.LOW)    # light if off by default
+  # Tuen lights on only when time is between timetoswitchon and timetoswitchoff
+  if now >= timetoswitchon and now <= timetoswitchoff:
+      GPIO.output(ledPin, GPIO.HIGH)
+      
+GPIO.cleanup(); #Clean up when exiting the program
+
