9d51a75809bf920dc1cb86e1adfeacf975b4d6fb
Nilay Arangil <50153327+nilayarangil@users.noreply.github.com>
2019-06-26 21:32:24 -0500

    Update fishtanklight.py

--- fishtanklight.py
+++ fishtanklight.py
@@ -2,11 +2,6 @@ import RPi.GPIO as GPIO #Use GPIO library
 import datetime #Use datetime library to get current time
 
 GPIO.setwarnings(False)
-# Set time when to switch the light on
-timetoswitchon = datetime.datetime.now().replace(hour=12,minute=23,second=0,microsecond=0)
-# Set time when to switch the light off
-timetoswitchoff = datetime.datetime.now().replace(hour=12,minute=25,second=0,microsecond=0) 
-
 ledPin = 11    # pin11 is connected to the led anode (+ve pin)
 GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 GPIO.setup(ledPin, GPIO.OUT)   # Set ledPin's mode as output
@@ -14,6 +9,11 @@ GPIO.output(ledPin, GPIO.HIGH) # Set ledPin high(+3.3V) to turn on led
 
 while True: # Continue looping indefinitely
   now = datetime.datetime.now()    # Get the current time
+  # Set time when to switch the light on
+  timetoswitchon = datetime.datetime.now().replace(hour=12,minute=23,second=0,microsecond=0)
+  # Set time when to switch the light off
+  timetoswitchoff = datetime.datetime.now().replace(hour=12,minute=25,second=0,microsecond=0) 
+  
   GPIO.output(ledPin, GPIO.LOW)    # light if off by default
   # Tuen lights on only when time is between timetoswitchon and timetoswitchoff
   if now >= timetoswitchon and now <= timetoswitchoff:
